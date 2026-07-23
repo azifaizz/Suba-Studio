dade10dadac0b042f85efa83a6414a627ecb074b
aseelazee <aseelfaizzin1@gmail.com>
2026-07-23 11:56:51 +0530

    Add smart, global, minimal Back button component

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import BackButton from '../ui/BackButton';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen relative">
             <Navbar />
+            <BackButton />
             <main className="flex-grow">
                 {children}
             </main>
